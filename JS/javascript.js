f2db9653fa5bbdb56ccfa7222969674f013b895c
Axel Rodríguez Criollo <47278265+axelandreR@users.noreply.github.com>
2021-11-03 11:42:29 -0500

    Javascript Act1.0

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,13 +1,25 @@
 'use strict'
 
 window.addEventListener('load', function(){
-    let aviso_error = document.getElementsByClassName("avisoError");
-    let aviso_error_Billetes = document.getElementsByClassName("avisoErrorBilletes");
+    let formulario = document.querySelector('#form');
+    
+    function sumaElementos(array){
+        let suma = 0;
+        for(let i=0; i<array.length;i++){
+            suma = suma + (Number(array[i].value));
+        }
+        return suma;
+    }; 
 
-    for(let i=0;i<aviso_error.length;i++){
-        aviso_error[i].style.display= "none";       
-    }
-    for(let j=0;j<aviso_error_Billetes.length;j++){
-        aviso_error_Billetes[j].style.display= "none";
-    }
+
+    formulario.addEventListener('submit', function(){
+        let monedas = document.getElementsByClassName('monedas');
+        let sumaMonedas = sumaElementos(monedas);
+
+        let billetes = document.getElementsByClassName('billetes');
+        let sumaBilletes = sumaElementos(billetes);
+
+        
+        console.log(sumaMonedas);
+    })
 });
