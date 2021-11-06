91cc5450b2c09d02dcea08d2cccd096c533aa39e
Axel Rodríguez Criollo <47278265+axelandreR@users.noreply.github.com>
2021-11-06 06:57:21 -0500

    CSS-Caja

--- JS/javascript.js
+++ JS/javascript.js
@@ -3,6 +3,22 @@
 window.addEventListener('load', function(){
     let formulario = document.querySelector('#form');
     
+
+
+    // Ocultar avisos de error
+    let aviso_error = document.getElementsByClassName("avisoError");
+    let aviso_error_Billetes = document.getElementsByClassName("avisoErrorBilletes");
+
+    for(let i=0;i<aviso_error.length;i++){
+        aviso_error[i].style.display= "none";       
+    }
+    for(let j=0;j<aviso_error_Billetes.length;j++){
+        aviso_error_Billetes[j].style.display= "none";
+    }
+
+    
+    // Suma de Monedas y Billetes
+
     function sumaElementos(array){
         let suma = 0;
         for(let i=0; i<array.length;i++){
