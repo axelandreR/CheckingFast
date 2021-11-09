46f2c88e38d514e01b696fd119cc7f56dde437ed
Axel Rodríguez Criollo <47278265+axelandreR@users.noreply.github.com>
2021-11-09 05:20:35 -0500

    New Interface 1.0

--- JS/javascript.js
+++ JS/javascript.js
@@ -16,7 +16,7 @@ window.addEventListener('load', function(){
         aviso_error_Billetes[j].style.display= "none";
     }
 
-    
+
     // Suma de Monedas y Billetes
 
     function sumaElementos(array){
@@ -27,6 +27,105 @@ window.addEventListener('load', function(){
         return suma;
     }; 
 
+    function validacionMonedas(elemento){
+        for(let i=0;i<elemento.length;i++){
+            let contenido = elemento[i].value;
+            console.log(contenido);
+            if(i == 0){
+                if(Number(contenido) % 1 != 0){
+                    aviso_error[i].style.display="block";
+                    elemento[i].value = " ";
+                    return true;
+                }else{
+                    aviso_error[i].style.display="none";
+                }
+            }else if(i ==1){
+                if(Number(contenido) % 2 != 0){
+                    aviso_error[i].style.display="block";
+                    elemento[i].value = " ";
+                    return true;
+                }else{
+                    aviso_error[i].style.display="none";
+                }
+            }else if(i == 2){
+                if(Number(contenido) % 5 != 0){
+                    aviso_error[i].style.display="block";
+                    elemento[i].value = " ";
+                    return true;
+                }else{
+                    aviso_error[i].style.display="none";
+                }
+            }else if(i == 3){
+                if((Number(contenido)*10) % 5 != 0){
+                    aviso_error[i].style.display="block";
+                    elemento[i].value = " ";
+                    return true;
+                }else{
+                    aviso_error[i].style.display="none";
+                }
+            }else if(i == 4){
+                if((Number(contenido)*10) % 2 != 0){
+                    aviso_error[i].style.display="block";
+                    elemento[i].value = " ";
+                    return true;
+                }else{
+                    aviso_error[i].style.display="none";
+                }
+            }else if(i == 5){
+                if((Number(contenido)*10) % 1 != 0){
+                    aviso_error[i].style.display="block";
+                    elemento[i].value = " ";
+                    return true;
+                }else{
+                    aviso_error[i].style.display="none";
+                }
+            }else{
+                return false;
+            }
+        }
+    }
+
+    function validacionBilletes(elemento2){
+        for(let i=0;i<elemento2.length;i++){
+            let contenido = elemento2[i].value;
+            console.log(contenido);
+            if(i == 0){
+                if(Number(contenido) % 10 != 0){
+                    aviso_error_Billetes[i].style.display="block";
+                    elemento2[i].value = " ";
+                    return true;
+                }else{
+                    aviso_error_Billetes[i].style.display="none";
+                }
+            }else if(i == 1){
+                if(Number(contenido) % 20 != 0){
+                    aviso_error_Billetes[i].style.display="block";
+                    elemento2[i].value = " ";
+                    return true;
+                }else{
+                    aviso_error_Billetes[i].style.display="none";
+                }
+            }else if(i == 2){
+                if(Number(contenido) % 50 != 0){
+                    aviso_error_Billetes[i].style.display="block";
+                    elemento2[i].value = " ";
+                    return true;
+                }else{
+                    aviso_error_Billetes[i].style.display="none";
+                }
+            }else if(i == 3){
+                if(Number(contenido) % 100 != 0){
+                    aviso_error_Billetes[i].style.display="block";
+                    elemento2[i].value = " ";
+                    return true;
+                }else{
+                    aviso_error_Billetes[i].style.display="none";
+                }
+            }else{
+                return false;
+            }
+        }
+    }
 
     formulario.addEventListener('submit', function(){
         let monedas = document.getElementsByClassName('monedas');
